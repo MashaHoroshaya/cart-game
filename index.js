6b4d2c7e81eafc59af7aff83caec5d7d887f18a7
MashaHoroshaya <horoshaya.ma@yandex.ru>
2023-03-10 20:47:46 +0400

    рендер игрового поля и запоминание уровня сложности

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-APP = document.querySelector('.app');
+const app = document.querySelector('.app');
+window.level = '';
 
-window.application = {
-    blocks: {},
-    screens: {},
-    timers: [],
+const renderLevel = () => {
+    app.innerHTML = '';
 
-    renderScreen: function(screenName) {
-        
-         window.application.timers.forEach(id => {
-           clearInterval(id);
-         });
+    const div = document.createElement('div');
+    div.classList.add('div_block');
+    div.innerHTML = `
+        <h1 class="h1_title">Выбери сложность</h1>
+        <div class="level">
+            <div class="level_item" data-level="1">1</div>
+            <div class="level_item" data-level="2">2</div>
+            <div class="level_item" data-level="3">3</div>
+        </div>
+        <button class="btn btn_start">Старт</button>
+    `;
+
+    app.append(div);
+}
+renderLevel();
+
+const renderGame = (level) => {
+    app.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.classList.add('game');
+    div.innerHTML = `Игровое поле уровня ${level}`;
+    app.append(div);
+}
 
 
-        if (window.application.screens[screenName]) {
-          APP.innerHTML = '';
-            window.application.screens[screenName]()
+app.addEventListener('click', (e) => {
+    if (e.target.classList.contains('level_item')) {
+        window.level = e.target.dataset.level;
+        console.log(window.level);
+    }
+
+    if (e.target.classList.contains('btn_start')) {
+        if (window.level) {
+            renderGame(window.level);
         } else {
-            console.log('Такого экрана блока не существует');
+            console.log('Сложность не выбрана');
         }
-       
-    },
+    }
+})
 
-    renderBlock: function(blockName, container) {
-        if (window.application.blocks[blockName]) {
-             
-            window.application.blocks[blockName](container);  
-            // Вот тут не понимаю строчку 
-        } else {
-            console.log('Такого блока не существует');
-        }
-        
-    },
-   
-  };
